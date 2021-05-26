7543d5b5782888c9f3a7b15987264455d47a6b14
Jacob <jbodera@uwaterloo.ca>
2021-05-26 10:50:46 -0400

    All constant member functions added and currently working on the non-constant member functions.

--- sequential-list.cpp
+++ sequential-list.cpp
@@ -3,46 +3,63 @@
 
 SequentialList::SequentialList(unsigned int cap)
 {
-    
+    data_ = new DataType[cap];
 
 }
 
 SequentialList::~SequentialList()
 {
-    
+    delete [] data_;
 
 }
 
 unsigned int SequentialList::size() const
 {
-    
+    unsigned int i = 0;
+    while (*(data_ + i) != NULL){
+        i++;
+    }
+    return i;
 }
 
 unsigned int SequentialList::capacity() const
 {
-    
+    return capacity_;
 }
 
 bool SequentialList::empty() const
 {
-    
+    for(int i = 0; i < capacity_; i++){
+        if(*(data_ + i) != NULL)
+            return false;
+    }
+    return true;
 }
 
 bool SequentialList::full() const
 {
-    
+    for(int i = 0; i < capacity_; i++){
+        if(*(data_ + i) = NULL)
+            return false;
+    }
+    return true;
 }
 
 SequentialList::DataType SequentialList::select(unsigned int index) const
 {
-   
-   
+   if(index >= 0 && index < capacity_)
+       return *(data_ + index);
+   return *(data_ + capacity_ - 1);
 
 }
 
 unsigned int SequentialList::search(DataType val) const
 {
- 
+    for(unsigned int i = 0; i < capacity_; i++){
+        if(*(data_ + i) == val)
+            return i;
+    }
+    return SequentialList::data_.size();
 
 }
 
@@ -53,9 +70,17 @@ void SequentialList::print() const
 
 bool SequentialList::insert(DataType val, unsigned int index)
 {
-  
-
-
+    int temp = 0;
+    int assign = val;
+    if(*(data_ + index - 1) != NULL || index == 0){
+        for(int i = 0; i <= data_.size() + 1; i++){
+            temp = *(data_ + index + i);
+            *(data_ + index + i) = assign;
+            assign = temp;
+        }
+        return true;
+    }
+    return false;
 }
 
 bool SequentialList::insert_front(DataType val)
