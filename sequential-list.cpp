16e029ba56b5be1bf2290a9efc4cfa4f55c625f7
Jacob <jbodera@uwaterloo.ca>
2021-05-26 11:20:45 -0400

    Added more non-constant member functions.

--- sequential-list.cpp
+++ sequential-list.cpp
@@ -10,7 +10,7 @@ SequentialList::SequentialList(unsigned int cap)
 SequentialList::~SequentialList()
 {
     delete [] data_;
-
+    data_ = NULL;
 }
 
 unsigned int SequentialList::size() const
@@ -59,7 +59,7 @@ unsigned int SequentialList::search(DataType val) const
         if(*(data_ + i) == val)
             return i;
     }
-    return SequentialList::data_.size();
+    return SequentialList::size();
 
 }
 
@@ -70,10 +70,13 @@ void SequentialList::print() const
 
 bool SequentialList::insert(DataType val, unsigned int index)
 {
+    if(size() == capacity_)
+        return false;
+
     int temp = 0;
     int assign = val;
     if(*(data_ + index - 1) != NULL || index == 0){
-        for(int i = 0; i <= data_.size() + 1; i++){
+        for(int i = 0; i <= size() + 1; i++){
             temp = *(data_ + index + i);
             *(data_ + index + i) = assign;
             assign = temp;
@@ -85,14 +88,26 @@ bool SequentialList::insert(DataType val, unsigned int index)
 
 bool SequentialList::insert_front(DataType val)
 {
+    if(size() == capacity_)
+        return false;
 
-
+    int temp = 0;
+    int assign = val;
+    for(int i = 0; i < capacity_; i++){
+        temp = *(data_ + i);
+        *(data_ + i) = assign;
+        assign = temp;
+    }
+    return true;
 }
 
 bool SequentialList::insert_back(DataType val)
 {
+    if(size() == capacity_)
+        return false;
 
-
+    *(data_ + size() + 1) = val;
+    return true;
 }
 
 bool SequentialList::remove(unsigned int index)
@@ -103,16 +118,29 @@ bool SequentialList::remove(unsigned int index)
 
 bool SequentialList::remove_front()
 {
-   
+    if(size() == 0)
+        return false;
+
+    *data_ = NULL;
+    data_ += 1;
+    return true;
 }
 
 bool SequentialList::remove_back()
 {
-   
+   if(size() == 0)
+       return false;
+
+   *(data_ + size() + 1) = NULL;
+   return true;
 }
 
 bool SequentialList::replace(unsigned int index, DataType val)
 {
-
+    if(*(data_ + index -1) != NULL || index == 0){
+        *(data_ + index) = val;
+        return true;
+    }
+    return false;
 }
 
